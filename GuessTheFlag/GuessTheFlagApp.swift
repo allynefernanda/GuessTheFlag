9c55f0feb51551bb609109006c5e6a54a6c29326
allynefernanda <fernandavsob@gmail.com>
2024-02-29 15:13:16 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by allyne fernanda  on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
